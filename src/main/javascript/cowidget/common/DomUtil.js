ea4cb4225de938f1b64e01d73ab0d609d4d8e974
Raw Lin <RawJL.Lin@gmail.com>
2019-10-20 11:20:47 +0800

    DomUtil

---
+++ src/main/javascript/cowidget/common/DomUtil.js
@@ -0,0 +1,33 @@
+/**
+ * CoWidget (c) Copyright 2019 RawYa HOME. Licensed under the Apache License, Version 2.0 - see LICENSE.
+ * 
+ * <pre>
+ * This is an optimized version of CoWidget, built for deployment and not for development.
+ * To get sources and documentation, please visit: http://cowidget.rawya.net
+ * </pre>
+ */
+class DomUtil  {
+		
+	static byId(id, doc) {
+			try{
+	        	// dojo.byId(id, doc);
+	        	
+	        	return ((typeof id == "string") ? (doc || CoWidget.container.document).getElementById(id) : id) || null; // DOMNode
+			}catch(exception) {
+				console.error('');
+			}
+     }
+        
+	static query(selecter) {
+     }
+        
+	static ready(priority, context, callback) {
+		try {
+		    dojo.ready(priority, context, callback);
+		} catch (exception) {
+		    console.error('[DomUtil.ready] exception: ', exception);
+	    } finally {
+	
+	    }
+	}
+}
